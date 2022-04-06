2c50b0621ab53dbd047cdf93383c0262a02492f7
Antonio <antonio.jpsalves@gmail.com>
2022-04-06 01:34:02 -0300

    Adicionando css module para evitar duplicidade/sobrescrita de css nas classes

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Botao from "../button";
-import './style.scss';
+import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
     render() {
         return(
-            <form className="novaTarefa">
-                <div className="inputContainer">
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um item para estudar: </label>
                     <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar hoje?" required />
                 </div>
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo de estudo: </label>
                     <input type="time" name="tempo" id="tempo" step="1" min="00:00:00" max="01:30:00" required />
                 </div>
